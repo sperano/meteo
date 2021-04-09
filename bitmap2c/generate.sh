b745aad765490db491ba863263c223cdf1cbee3e
Eric Sperano <eric.sperano@gmail.com>
2021-04-08 22:32:22 -0700

    added reference png

--- bitmap2c/generate.sh
+++ bitmap2c/generate.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 go build
 
-cat ../bitmaps/02n.bmp | ./bitmap2c  e6e6dfdf0000ffff=13 >../bitmaps/02n.h
+cat ../bitmaps/02n.bmp | ./bitmap2c  e6e6dfdf0000ffff=13 >../bitmaps/02n.c
